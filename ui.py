ebbf14a8767262a6eb17f14f7e691ed024ea22c9
Chris Small <smallchris19@gmail.com>
2023-07-22 20:53:20 -0600

    Did some UI shit

--- ui.py
+++ ui.py
@@ -2,27 +2,64 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+from functools import partial
 from kivy.uix.label import Label
+from kivy.core.window import Window
 from ai import process_user_message
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
 
-class ChatUI(BoxLayout):
+class ChatInterface(BoxLayout):
     def __init__(self, **kwargs):
-        super(ChatUI, self).__init__(**kwargs)
+        super(ChatInterface, self).__init__(**kwargs)
         self.orientation = 'vertical'
-        self.user_input = TextInput(hint_text='Type your message here...')
-        self.submit_button = Button(text='Submit', on_press=self.send_user_message)
-        self.ai_response_label = Label(text='', size_hint_y=None, height=50)
 
+        self.messages = []
+        self.ai_response = "Hello! I am the AI. How can I assist you today?"
+
+        self.chat_label = Label(text="", markup=True)
+        self.user_input = TextInput(hint_text='Type your message here...', size_hint_y=None, height=50)
+        self.send_button = Button(text='Send', size_hint_y=None, height=50)
+        self.send_button.bind(on_press=self.send_user_message)
+
+        self.add_widget(self.chat_label)
         self.add_widget(self.user_input)
-        self.add_widget(self.submit_button)
-        self.add_widget(self.ai_response_label)
+        self.add_widget(self.send_button)
 
     def send_user_message(self, instance):
         user_message = self.user_input.text.strip()
         if user_message:
             # Call the function from the AI file to process the user's message and get AI response
             ai_response = process_user_message(user_message)
+            self.messages.append("[color=00FF00]You:[/color] " + user_message)
+            self.messages.append(ai_response)
+            self.update_chat_label()
+            self.user_input.text = ""
+
+    def update_chat_label(self):
+        self.chat_label.text = "\n\n".join(self.messages)
+
+class ChatApp(App):
+    def build(self):
+        return ChatInterface()
+
+if __name__ == '__main__':
+    ChatApp().run()
+
+
+    def send_user_message(self, instance):
+        user_message = self.user_input.text.strip()
+        if user_message:
+            self.previousChat.append(user_message)
+            # Call the function from the AI file to process the user's message and get AI response
+            ai_response = process_user_message(user_message)
             self.ai_response_label.text = ai_response
+
+            # Display user message history above AI response
+            user_message_history = f"You:\n".join(self.previousChat)
+            self.ai_response_label.text = f"{user_message_history}\n\nAI:\n{ai_response}"
+            self.user_input.text = ''
 
 if __name__ == '__main__':
     class ChatApp(App):
@@ -30,3 +67,5 @@ if __name__ == '__main__':
             return ChatUI()
 
     ChatApp().run()
+
+
